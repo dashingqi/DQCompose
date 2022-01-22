5fc1efcbabf2564b8b9bd9c4bbd48fe61c5adf73
zhangqi61 <zhangqi61@baidu.com>
2022-01-22 23:27:24 +0800

    [update]

--- app/src/main/java/com/dashingqi/dqcompose/MainActivity.kt
+++ app/src/main/java/com/dashingqi/dqcompose/MainActivity.kt
@@ -3,11 +3,16 @@ package com.dashingqi.dqcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.dashingqi.dqcompose.ui.theme.DQComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,5 +39,20 @@ fun Greeting(name: String) {
 fun DefaultPreview() {
     DQComposeTheme {
         Greeting("Android")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HelloContent() {
+    Column(modifier = Modifier.padding(16.dp)) {
+        Text(
+            text = "Hello",
+            modifier = Modifier.padding(bottom = 8.dp),
+            style = MaterialTheme.typography.h5
+        )
+        OutlinedTextField(value = "heiha", onValueChange = {},
+            label = { Text(text = "name") })
+
     }
 }
