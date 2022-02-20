5a23c65a1825b5dbccf40128f1ca108c0cf1f007
zhangqi61 <zhangqi61@baidu.com>
2022-02-20 11:13:34 +0800

    [update]

--- app/src/main/java/com/dashingqi/dqcompose/MainActivity.kt
+++ app/src/main/java/com/dashingqi/dqcompose/MainActivity.kt
@@ -3,11 +3,16 @@ package com.dashingqi.dqcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.dashingqi.dqcompose.ui.theme.DQComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +22,8 @@ class MainActivity : ComponentActivity() {
             DQComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+//                    Greeting("Android")
+                    MessageList()
                 }
             }
         }
@@ -34,5 +40,24 @@ fun Greeting(name: String) {
 fun DefaultPreview() {
     DQComposeTheme {
         Greeting("Android")
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun MessageList() {
+    LazyColumn(
+        // 内容边缘添加内边距
+        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
+        // 列表项之间添加间距
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        // 实验性
+        stickyHeader {
+            Text(text = "这是一个粘性标题")
+        }
+        items(50) { index ->
+            Text(text = "Item $index")
+        }
     }
 }
