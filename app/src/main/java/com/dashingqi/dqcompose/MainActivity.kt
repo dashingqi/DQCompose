309d913c93404b970a0659e8848c338f4307000f
zhangqi61 <zhangqi61@baidu.com>
2022-03-06 17:37:41 +0800

    [TabRow的使用]

--- app/src/main/java/com/dashingqi/dqcompose/MainActivity.kt
+++ app/src/main/java/com/dashingqi/dqcompose/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.dashingqi.dqcompose.ui.theme.DQComposeTheme
+import com.dashingqi.dqcompose.widget.TabRowSample
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,7 +24,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
 //                    Greeting("Android")
-                    MessageList()
+                    TabRowSample()
                 }
             }
         }
